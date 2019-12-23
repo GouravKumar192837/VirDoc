cb8f4cdd36d6ada8c3fc56a0ae9a5105a45e890b
cheersanimesh <cheersanimesh@gmail.com>
2019-12-23 14:05:52 +0530

    Changed the classifier according to the demands
    Json file attachment added..

--- disease-classifier/classifier.py
+++ disease-classifier/classifier.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+import json
 data= pd.read_csv("datasets/Training.csv")
 df= pd.DataFrame(data)
 cols= df.columns
@@ -15,6 +16,15 @@ y= df['prognosis']
 x_train,x_test,y_train,y_test= train_test_split(x,y,test_size=0.33,random_state=42)
 mnb= MultinomialNB()
 mnb.fit(x_train,y_train)
+##the following code has to be deployed to read from json file and feed it to ML model
+'''
+with open(//json file path) as json_file:
+    data_json_temp= json.load(json_file)
+## change the property accordingly
+data_json_string= data_json_temp[##property]
+arr_json_string= data_json_string.split(";")
+
+''' 
 
 ##the following code is for after the disease has been predicted
 data1= pd.read_csv("datasets/MANUAL SET complete - Sheet1.csv")
@@ -23,9 +33,19 @@ data=data.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3','Unnamed: 5', 'Unnamed:
 data=data.drop([0])
 data3=data.set_index('DISEASE')
 
+##this code generates the sample_x
+'''
+sample_x_temp=[]
+for i in len(cols):
+    if(arr_json_string[i]==cols[i]):
+        sample_x_temp.append(1)
+    else:
+        sample_x_temp.append(0)
+        
+'''
 '''
 ## predicting module
-sample_x=[]
+sample_x= sample_x_temp
 disease1=mnb.predict([sample_x])
 disease= disease1[0]
 '''
@@ -33,6 +53,13 @@ disease= disease1[0]
 ## disease_values= data3.loc[disease].values
 ## link_internet= disease_values[0]
 ## specalist = disease_values[1]
+### the module now converts the results into a JSON file
+'''
+##this part needs more attention
+results={'disease':disease_values,'link':link_internet,'specalist': disease_values[1]}
+with open(//json file address,w) as json_file2:
+    json.dump(//results,json_file2)
+'''
 '''
 ##for testing the test data
 test_data= pd.read_csv("datasets/Testing.csv")
